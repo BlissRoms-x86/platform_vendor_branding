159af7185c96c592fbb2b5bf9b39dd118f4e248e
Michael Bestas <mkbestas@gmail.com>
2021-06-11 19:32:26 +0300

    Don't copy privapp-permissions-goldfish.xml for GSI targets
    * It is already copied to product partition through AOSP makefiles
    
    Change-Id: I9d877728decc712ce6ae5cc285b1b6a7a4903daf

--- build/target/product/lmodroid_generic_target.mk
+++ build/target/product/lmodroid_generic_target.mk
@@ -16,8 +16,5 @@
 $(call inherit-product, build/target/product/gsi_release.mk)
 $(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
-PRODUCT_COPY_FILES += \
-    device/generic/goldfish/data/etc/permissions/privapp-permissions-goldfish.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-goldfish.xml \
-
 # Allow building otatools
 TARGET_FORCE_OTA_PACKAGE := true
