6aa7303a98ce7af2457b5e9cf5029f74cce00385
Alexander Koskovich <zvnexus@outlook.com>
2021-08-03 09:20:38 -0700

    Revert "lmodroid: Exclude all lmodroid overlays from RRO"
     * Now that PRODUCT_PACKAGE_OVERLAYS is used instead it's fine
       for Lineage overlays to be in RROs, they won't end up on
       /vendor.
    
    This reverts commit e4868ccb6b1e7c60621c68b738703d979f65af42.
    
    Change-Id: I26cb53f6b773821cbb08e4eb309f88d736a31609
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- config/common.mk
+++ config/common.mk
@@ -201,7 +201,6 @@ endif
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
 
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lmodroid/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/lmodroid/overlay/common
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lmodroid/config/device_framework_matrix.xml
