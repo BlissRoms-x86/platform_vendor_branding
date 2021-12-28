LineageOS/android_vendor_lineage

build: Add x86/x86_64 emulator targets
Change-Id: Ic8789ad2072b9ebda91f9b4a7c288a5a9bcec3fd

build/target/product/AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2020 The LineageOS Project
+# Copyright (C) 2018-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,7 +28,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_tv_x86.mk \
     $(LOCAL_DIR)/lineage_tv_x86_64.mk \
     $(LOCAL_DIR)/lineage_car_arm64.mk \
-    $(LOCAL_DIR)/lineage_car_x86_64.mk
+    $(LOCAL_DIR)/lineage_car_x86_64.mk \
+    $(LOCAL_DIR)/lineage_sdk_phone_x86.mk \
+    $(LOCAL_DIR)/lineage_sdk_phone_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_arm-userdebug \
@@ -44,4 +46,6 @@ COMMON_LUNCH_CHOICES := \
     lineage_tv_x86-userdebug \
     lineage_tv_x86_64-userdebug \
     lineage_car_arm64-userdebug \
-    lineage_car_x86_64-userdebug
+    lineage_car_x86_64-userdebug \
+    lineage_sdk_phone_x86-userdebug \
+    lineage_sdk_phone_x86_64-userdebug

BlissRoms-x86/platform_vendor_branding

build/target/product/AndroidProducts.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The LibreMobileOS Foundation
+# Copyright (C) 2020-2021 The LibreMobileOS Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,7 +25,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lmodroid_tv_x86.mk \
     $(LOCAL_DIR)/lmodroid_tv_x86_64.mk \
     $(LOCAL_DIR)/lmodroid_car_arm64.mk \
-    $(LOCAL_DIR)/lmodroid_car_x86_64.mk
+    $(LOCAL_DIR)/lmodroid_car_x86_64.mk \
+    $(LOCAL_DIR)/lmodroid_sdk_phone_x86.mk \
+    $(LOCAL_DIR)/lmodroid_sdk_phone_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     lmodroid_arm-userdebug \
@@ -37,4 +39,5 @@ COMMON_LUNCH_CHOICES := \
     lmodroid_tv_x86-userdebug \
     lmodroid_tv_x86_64-userdebug \
     lmodroid_car_arm64-userdebug \
-    lmodroid_car_x86_64-userdebug
+    lmodroid_sdk_phone_x86-userdebug \
+    lmodroid_sdk_phone_x86_64-userdebug