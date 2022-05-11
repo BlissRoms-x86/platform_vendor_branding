LineageOS/android_vendor_lineage

Rename automotive emulator targets
Matches the upstream separation of gsi and emulator targets.

Change-Id: I07947727c39b87d2bd1ee6bf2809a7f40ff380ba

build/target/product/AndroidProducts.mk
@@ -23,8 +23,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_tv_arm64.mk \
     $(LOCAL_DIR)/lineage_tv_x86.mk \
     $(LOCAL_DIR)/lineage_tv_x86_64.mk \
-    $(LOCAL_DIR)/lineage_car_arm64.mk \
-    $(LOCAL_DIR)/lineage_car_x86_64.mk \
+    $(LOCAL_DIR)/lineage_sdk_car_arm64.mk \
+    $(LOCAL_DIR)/lineage_sdk_car_x86_64.mk \
     $(LOCAL_DIR)/lineage_sdk_phone_x86.mk \
     $(LOCAL_DIR)/lineage_sdk_phone_x86_64.mk
 
@@ -37,7 +37,7 @@ COMMON_LUNCH_CHOICES := \
     lineage_tv_arm64-userdebug \
     lineage_tv_x86-userdebug \
     lineage_tv_x86_64-userdebug \
-    lineage_car_arm64-userdebug \
-    lineage_car_x86_64-userdebug \
+    lineage_sdk_car_arm64-userdebug \
+    lineage_sdk_car_x86_64-userdebug \
     lineage_sdk_phone_x86-userdebug \
     lineage_sdk_phone_x86_64-userdebug

BlissRoms-x86/platform_vendor_branding

Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

build/target/product/AndroidProducts.mk
@@ -24,8 +24,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lmodroid_tv_arm64.mk \
     $(LOCAL_DIR)/lmodroid_tv_x86.mk \
     $(LOCAL_DIR)/lmodroid_tv_x86_64.mk \
-    $(LOCAL_DIR)/lmodroid_car_arm64.mk \
-    $(LOCAL_DIR)/lmodroid_car_x86_64.mk \
+    $(LOCAL_DIR)/lmodroid_sdk_car_arm64.mk \
+    $(LOCAL_DIR)/lmodroid_sdk_car_x86_64.mk \
     $(LOCAL_DIR)/lmodroid_sdk_phone_x86.mk \
     $(LOCAL_DIR)/lmodroid_sdk_phone_x86_64.mk
 
@@ -38,6 +38,7 @@ COMMON_LUNCH_CHOICES := \
     lmodroid_tv_arm64-userdebug \
     lmodroid_tv_x86-userdebug \
     lmodroid_tv_x86_64-userdebug \
-    lmodroid_car_arm64-userdebug \
+    lmodroid_sdk_car_arm64-userdebug \
+    lmodroid_sdk_car_x86_64-userdebug \
     lmodroid_sdk_phone_x86-userdebug \
     lmodroid_sdk_phone_x86_64-userdebug