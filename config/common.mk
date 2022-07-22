PixelExtended/vendor_aosp

vendor: introduce app lock [4/4]

config/common.mk
@@ -40,6 +40,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/aosp/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
 
+# app lock permission
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # Copy all custom init rc files
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/init/init.pixelexperience-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.pixelexperience-updater.rc

BlissRoms-x86/platform_vendor_branding

Change-Id: I1ee3b68d5874c3d233ea08ae210cb02990c43f5d

config/common.mk
@@ -52,7 +52,7 @@ endif
 
 # Some permissions
 PRODUCT_COPY_FILES += \
-    vendor/lmodroid/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
+    vendor/lmodroid/config/permissions/privapp-permissions-lmodroid.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lmodroid.xml
 
 # lmodroid specific init rc file
 PRODUCT_COPY_FILES += \